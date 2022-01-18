58b9f804783f95bb9ff6ea3204e60ec292f4f722
boby-crack <bobymaulanaa21@gmail.com>
2022-01-18 22:08:12 +0700

    upload file

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <TeknikHeader />
+    <HeroCarousel />
+    <WomenBanner />
+    <InstaTeknik />
+    <PartnerTeknik />
+    <FooterTeknik />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TeknikHeader from '@/components/TeknikHeader.vue';
+import HeroCarousel from '@/components/HeroCarousel.vue';
+import WomenBanner from '@/components/WomenBanner.vue';
+import InstaTeknik from '@/components/InstaTeknik.vue';
+import PartnerTeknik from '@/components/PartnerTeknik.vue';
+import FooterTeknik from '@/components/FooterTeknik.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    TeknikHeader,
+    HeroCarousel,
+    WomenBanner,
+    InstaTeknik,
+    PartnerTeknik,
+    FooterTeknik,
+  },
+};
+</script>
